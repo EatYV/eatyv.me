5d75621ffe03d897f0f373fccdb0c15a9f207201
Salanoid <salanoid@gmail.com>
2017-03-28 22:33:58 +0000

    good routes for users and recipes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
-  resources :recipes
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'recipes#index'
   
-  resources :users do
-    resources :recipes
-  end
+  resources :recipes
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170328151941) do
+ActiveRecord::Schema.define(version: 20170328215412) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,16 @@ ActiveRecord::Schema.define(version: 20170328151941) do
     t.datetime "updated_at",  null: false
     t.integer  "user_id"
     t.index ["user_id"], name: "index_recipes_on_user_id", using: :btree
+  end
+
+  create_table "reservations", force: :cascade do |t|
+    t.integer  "no_of_users"
+    t.integer  "user_id"
+    t.integer  "recipe_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.index ["recipe_id"], name: "index_reservations_on_recipe_id", using: :btree
+    t.index ["user_id"], name: "index_reservations_on_user_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
@@ -44,4 +54,6 @@ ActiveRecord::Schema.define(version: 20170328151941) do
   end
 
   add_foreign_key "recipes", "users"
+  add_foreign_key "reservations", "recipes"
+  add_foreign_key "reservations", "users"
 end
