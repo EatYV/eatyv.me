e160ff9a40c333432089a63888352cbd5e502a78
Salanoid <salanoid@gmail.com>
2017-03-28 15:57:32 +0000

    settings, sign_in, sign_out, sign_up buttons, recipes/index as root page

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,4 @@
 class Recipe < ApplicationRecord
     belongs_to :user
+    validates :title, :price, :duration, :description, :user_id, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,5 @@ Rails.application.routes.draw do
   resources :recipes
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  devise_scope :user do
-    root to: "devise/sessions#new"  
-  end
+  root 'recipes#index'
 end
