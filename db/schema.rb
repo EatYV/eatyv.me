458875ea9e7fec8702fe67e2bd44fbec7205571f
Salanoid <salanoid@gmail.com>
2017-03-29 21:31:04 +0000

    first deploy on heroku

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170329100653) do
+ActiveRecord::Schema.define(version: 20170329120614) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,10 @@ ActiveRecord::Schema.define(version: 20170329100653) do
     t.text     "message"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
+    t.integer  "recipe_id"
+    t.index ["recipe_id"], name: "index_reservations_on_recipe_id", using: :btree
+    t.index ["user_id"], name: "index_reservations_on_user_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
@@ -54,4 +58,6 @@ ActiveRecord::Schema.define(version: 20170329100653) do
   end
 
   add_foreign_key "recipes", "users"
+  add_foreign_key "reservations", "recipes"
+  add_foreign_key "reservations", "users"
 end
