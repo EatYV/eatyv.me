213a26eb39bc6f616c652f87e58d1e134b93b864
Salanoid <salanoid@gmail.com>
2017-05-12 09:07:03 +0000

    carrierwave configurations

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,11 @@
+CarrierWave.configure do |config|
+  config.storage = :fog
+  config.fog_directory = 'eatyv'
+
+  config.fog_credentials = {
+    :provider => 'AWS',
+    :aws_access_key_id => 'AKIAI3SDNEXQDRXHZ5OQ',
+    :aws_secret_access_key => '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',
+    :region => 'EU'
+  }
+end
