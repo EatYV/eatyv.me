49f4549e4caf997d5977e6f735c9198c50fbe10e
Salanoid <salanoid@gmail.com>
2017-05-12 09:17:45 +0000

    carrierwave config v4

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,5 +6,6 @@ CarrierWave.configure do |config|
     :provider => 'AWS',
     :aws_access_key_id => 'AKIAI3SDNEXQDRXHZ5OQ',
     :aws_secret_access_key => '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',
+    :region => 'EU'
   }
 end
