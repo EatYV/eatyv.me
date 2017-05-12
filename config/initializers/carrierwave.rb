53686185a2247ded2862c748ca4ffeda01dde61f
Salanoid <salanoid@gmail.com>
2017-05-12 11:04:03 +0000

    carrierwave config v7

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,19 +1,11 @@
-if Rails.env.development? or Rails.env.test?
-  CarrierWave.configure do |config|
-    config.storage = :file
-    config.enable_processing = false
-  end
+CarrierWave.configure do |config|
+  config.fog_provider = 'fog/aws'
+  config.fog_directory = 'eatyv'
 
-else
-  CarrierWave.configure do |config|
-    config.fog_credentials = {
-      :provider               => 'AWS',            # required
-      :aws_access_key_id      => 'AKIAI3SDNEXQDRXHZ5OQ',     # required
-      :aws_secret_access_key  => 'PGnbdrSWv4gTBZiDbEh2wF9mSaaxWLVBoe',  # required
-      :region                 => 'eu-central-1'
-    }
-    config.fog_use_ssl_for_aws = false
-    config.storage             = :fog
-    config.fog_directory       = 'images'          # required
-  end
+  config.fog_credentials = {
+    :provider => 'AWS',
+    :aws_access_key_id => 'AKIAI3SDNEXQDRXHZ5OQ',
+    :aws_secret_access_key => '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',
+    :region => 'EU'
+  }
 end
